6cb987f28d190f5a7e4965de9b5624061a8257ee
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-18 17:36:28 +0200

    Update App.vue

--- vuejs-slack-clone/client/src/App.vue
+++ vuejs-slack-clone/client/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld v-if="!!server.status && server.status === 'ok'" msg="Welcome to Your Vue.js App"/>
-    <template v-else
+    <template v-else>
     <HelloWorld msg="Connecting..."/>
     </template>
   </div>
